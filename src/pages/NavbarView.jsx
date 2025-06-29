ed061f4b8e13035e4a30b375a3602fb16e2beefb
Survidia <developer.elias.essadak@gmail.com>
2025-06-29 11:29:46 +0200

    se pone el logo de survidia

--- src/pages/NavbarView.jsx
+++ src/pages/NavbarView.jsx
@@ -1,16 +1,27 @@
-import { AppBar, Toolbar, Typography, Button, Box } from "@mui/material";
+import { AppBar, Toolbar, Button, Box } from "@mui/material";
 import { Link } from "react-router-dom";
 import { colors } from "../theme/colors";
 
 const NavbarView = () => {
 	return (
-		<AppBar position="sticky" sx={{ backgroundColor: colors.primary }}>
+		<AppBar
+			position="sticky"
+			sx={{
+				backgroundColor: colors.primary,
+				height: 64, // altura fija
+				overflow: "hidden", // evita que se deforme por el logo
+			}}
+		>
 			<Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
-				<Typography variant="h6" color="inherit">
-					<Button component={Link} to="/api" sx={{ color: "#fff" }}>
-						Survidia
-					</Button>
-				</Typography>
+					<Box
+						component="img"
+					src="/survidia.png"
+						alt="Survidia Logo"
+						sx={{
+						height: 160,
+						ml: 2,
+						}}
+					/>
 				<Box>
 					<Button component={Link} to="/api" sx={{ color: "#fff" }}>
 						Gastos
